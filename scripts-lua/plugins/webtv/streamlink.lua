eb1626557864e5b7060baf420f8b67b9c47392f3
vanhofen <vanhofen@gmx.de>
2025-12-30 21:34:40 +0100

    webtv: just formatting fixes in streamlink.lua
    Origin commit data
    ------------------
    Branch: master
    Commit: https://github.com/neutrino-images/ni-neutrino-plugins/commit/c3318f2079f19fb8c54571a8b3a11ddb660a7eb3
    Author: vanhofen <vanhofen@gmx.de>
    Date: 2025-12-30 (Tue, 30 Dec 2025)
    
    Origin message was:
    ------------------
    - webtv: just formatting fixes in streamlink.lua
    
    ------------------
    No further description and justification available within origin commit message!
    
    ------------------
    This commit was generated by Migit

--- scripts-lua/plugins/webtv/streamlink.lua
+++ scripts-lua/plugins/webtv/streamlink.lua
@@ -5,54 +5,53 @@ local json = require "json"
 
 -- Hilfsfunktion: Shell-Befehl ausführen und Ausgabe zurückgeben
 local function pop(cmd)
-    local f = assert(io.popen(cmd, "r"))
-    local s = f:read("*a")
-    f:close()
-    return s
+	local f = assert(io.popen(cmd, "r"))
+	local s = f:read("*a")
+	f:close()
+	return s
 end
 
--------------------------------------- Streamlink ------------------------------------------
+--------------------------------- Streamlink ---------------------------------
 
 local url = arg[1]
 if not url then
-    print("Keine URL übergeben")
-    return nil
+	print("Keine URL übergeben")
+	return nil
 end
 
 local final_url = url
 
 -- stvp-Links auflösen
 if url:match("stvp") then
-    local cmd = string.format(
-        "curl -kLs -o /dev/null -w %%{url_effective} %q",
-        url
-    )
-    final_url = pop(cmd)
-    final_url = final_url:gsub("%s+$", "")
+	local cmd = string.format(
+		"curl -kLs -o /dev/null -w %%{url_effective} %q",
+		url
+	)
+	final_url = pop(cmd)
+	final_url = final_url:gsub("%s+$", "")
 end
 
--- Wenn nach dem curl nichts übrig ist → abbrechen
+-- Wenn nach dem curl nichts übrig ist -> abbrechen
 if not final_url or final_url == "" then
-    return nil
+	return nil
 end
 
 -- Streamlink: beste spielbare URL holen
 local qualities = "1080p,720p,3300k,2300k,2100k,best"
 local cmd = string.format(
-    "streamlink %q %s --stream-url",
-    final_url,
-    qualities
+	"streamlink %q %s --stream-url",
+	final_url,
+	qualities
 )
 
 local stream_url = pop(cmd)
-stream_url = stream_url:gsub("%s+$", "")   -- \n, \r, Leerzeichen entfernen
+stream_url = stream_url:gsub("%s+$", "") -- \n, \r, Leerzeichen entfernen
 
--- Wenn Streamlink nichts liefert → abbrechen
+-- Wenn Streamlink nichts liefert -> abbrechen
 if not stream_url or stream_url == "" then
-    return nil
+	return nil
 end
 
 -- JSON-Ausgabe
 local entry = { url = stream_url }
 return json:encode(entry)
-
